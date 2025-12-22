65c63f50750d1c05613e02f5b1a7f029bc9b7801
Manish kumar <Hairbymanish@gmail.com>
2025-12-22 13:18:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+const App = () => {
+  return (
+    <div className="main-div">
+
+
+
+
+
+     <div className="one">
+      <h2>manish</h2>
+
+      <p>about</p>
+      <p>about</p>
+      <p>about</p>
+
+      <input type="text" placeholder="i am manish" />
+      <button>Search</button>
+     </div>
+
+     <div className="two">div two </div>
+     <div className="three">div two </div>
+     <div className="four">div two </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+};
+
+export default App;

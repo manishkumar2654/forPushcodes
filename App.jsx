37064820a43f6a9fc2e511afd7921fb307121231
Manish kumar <Hairbymanish@gmail.com>
2025-12-25 10:39:05 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,32 +1,28 @@
 import "./App.css";
+
+
 const App = () => {
   return (
-    <div className="main-div">
 
 
 
 
 
-     <div className="one">
-      <h2>manish</h2>
+<div>
+  <h1>headding</h1>
+ <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque modi, hic nesciunt fugiat molestiae tenetur adipisci laudantium inventore sed aspernatur odit pariatur laborum itaque laboriosam mollitia magnam animi rerum quia.
+ Quis excepturi adipisci alias provident aliquid totam aut itaque molestiae ipsum? Quia rem ratione itaque id qui deserunt blanditiis a labore ducimus, similique laboriosam explicabo voluptates nesciunt nam ab soluta.
+ Nobis corrupti quisquam cupiditate at aut placeat labore rerum modi. Dicta necessitatibus, ad ab soluta libero, odio animi modi est id nostrum quae nam culpa dolorum perferendis iste aliquid reiciendis.
+ Et, officiis. Tenetur quasi voluptatem officiis quia impedit ullam harum placeat neque eligendi asperiores doloremque itaque nobis ducimus amet fuga pariatur eaque cumque fugit, iure consequuntur rem eius aliquid quidem!
+ Incidunt quibusdam architecto veritatis repellendus sequi maiores nostrum, consequatur eos provident expedita, illum voluptates dolore laboriosam laudantium. Ullam pariatur enim nobis officiis accusantium facilis odio? Quas maxime nesciunt aliquam quaerat.
+ Debitis molestias vitae rem cupiditate cumque maiores ab facere enim tenetur, veritatis officia iusto, perferendis dolor a tempora. Fuga, deleniti repellat quibusdam iste reiciendis perferendis quia doloribus? Velit, laborum maxime!
+ Aliquam fugiat repellendus, non nobis ducimus perspiciatis saepe sequi dicta mollitia eius obcaecati earum asperiores voluptatem, aut exercitationem quod suscipit. Aliquam iste quia officiis quis similique soluta ipsa vero quibusdam!
+ Aspernatur ducimus ab fuga quasi labore. A necessitatibus beatae sint explicabo. Dolorem sunt repellendus laborum iure veniam voluptatum alias doloribus modi. Nam, expedita animi excepturi est maiores et eligendi minus!
+ Earum blanditiis tenetur facilis eos consequuntur neque autem minus, sapiente accusantium quasi odit repellat voluptatibus, perferendis saepe corporis debitis nam. Ducimus quis dignissimos similique accusamus rem provident delectus sapiente at.
+ Rerum commodi, ex molestiae sunt asperiores incidunt placeat magnam libero. Aspernatur dignissimos voluptas omnis ducimus fugit iste. Repellat eius nulla magnam cupiditate, tempora unde temporibus. Inventore iste pariatur unde sed.</p>
 
-      <p>about</p>
-      <p>about</p>
-      <p>about</p>
-
-      <input type="text" placeholder="i am manish" />
-      <button>Search</button>
-     </div>
-
-     <div className="two">div two </div>
-     <div className="three">div two </div>
-     <div className="four">div two </div>
-
-
-
-
-
-
+  
+</div>
 
 
 
@@ -35,9 +31,6 @@ const App = () => {
 
 
 
-
-
-    </div>
   );
 };
 
